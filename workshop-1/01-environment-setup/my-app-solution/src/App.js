d8ed1d1670db8e9e7236fc1e39afbc6f93793f65
ibrahimALANESI <ansi_epi@yahoo.com>
2022-05-13 01:25:44 +0300

    Initialize dependabot/npm_and_yarn/workshop-2/04-testing-code/unit-tests/ansi-regex-5.0.1

---
+++ workshop-1/01-environment-setup/my-app-solution/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './App.module.css'
+
+
+const MyApp = () => (
+    <div className={classes.container}>
+        <h1>Welcome to the Web App Academy 2021!</h1>
+    </div>
+)
+
+export default MyApp
